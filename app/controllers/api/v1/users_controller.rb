c44012daa5f0372f00bd7ea985e8cfabc74d4bb6
laurkim <laurakeem@gmail.com>
2017-11-30 19:25:47 -0500

    reset routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
-class API::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
+    binding.pry
     render json: User.all
+  end
+
+  def create
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
       resources :liked_artists
       resources :artists
       resources :users
+      # resources :logins
+
+      get 'login', to: "logins#create"
+      get 'userauth', to: "logins#redirectURL"
     end
   end
 end
