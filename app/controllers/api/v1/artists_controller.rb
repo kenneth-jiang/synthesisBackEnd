8afa338d0120b836e724de356d84fe37d9634249
laurkim <laurakeem@gmail.com>
2017-12-05 14:27:25 -0500

    limit api search results

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ArtistsController < ApplicationController
     # taking the access token stored in user and decoding it
     header = {'Authorization': "Bearer " + decoded[0]["access_token"]}
     # iterating through decoded to get to the access token and passing it into the header
-    artist_response = RestClient.get("https://api.spotify.com/v1/search?q=#{body[:artist]}&type=artist", header)
+    artist_response = RestClient.get("https://api.spotify.com/v1/search?q=#{body[:artist]}&limit=7&type=artist", header)
     artist_params = JSON.parse(artist_response.body)
     # making the fetch request to the spotify api and parsing it
     render json: {artists: artist_params}
